00ba3076fcc7e913bc5b017807c48ec473739e33
Gorazio Lane <Gorazio Lane>
2017-01-25 20:44:34 +0300

    add a lot of things :D

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    startAnimation(0);
     /* pagination */
     var paginationList = $('.pagination ul li'),
         menuList = $('.menu__list ul li'),
@@ -14,8 +15,10 @@ window.onload = function() {
     $.map(paginationList, function(el, index) {
         var page = $(el);
         page.on('click', function(evt) {
-            currentPageId = index;         
-            smoothScrollTo(pages[index].offset);
+            if (currentPageId !== index) {
+                currentPageId = index
+                smoothScrollTo(pages[index].offset);
+            }
         });
     });
     $.map(menuList, function(el, index) {
@@ -27,14 +30,47 @@ window.onload = function() {
         });
     });
 
+    /* window scrolling */
     var c = 1, // page scrolling up/down coef
         lastOffset = window.pageYOffset || document.documentElement.scrollTop;
+    
     window.onscroll = (evt) => {
         var scrolled = window.pageYOffset || document.documentElement.scrollTop;
         var c = lastOffset > scrolled ? -1 : 1;
             lastOffset = scrolled;
-        var newPageId = Math.round((scrolled + (c * 200)) / $(window).height());
-        
+        var newPageId = Math.round((scrolled + (c * 100)) / $(window).height());
+        changePage(newPageId);
+    };      
+
+    /* menu */
+    var menu = $('.navbar__menu');
+    $('.menu__btn').on('click', function() {
+        toggleMenu(menu);
+    });
+
+    /* more btn */
+    $('.more').on('click', function() {
+        changePage(1);
+        smoothScrollTo(pages[1].offset);
+    });
+
+    /* page animations */
+    function startAnimation(id) {
+        console.log(`page${id} animation`);
+        switch(id) {
+            case 0:
+                $('.slider').animate({
+                    'padding-left': '150px'
+                }, 900);
+                $('.logo').fadeIn('slow');
+                break;
+            default:
+                break;
+        };
+    };
+
+
+    function changePage(newPageId) {
         if (currentPageId !== newPageId) {
             $(paginationList[newPageId]).addClass('active');
             $(paginationList[currentPageId]).removeClass('active');
@@ -46,26 +82,22 @@ window.onload = function() {
                 $('.pagination').removeClass('pagination_inverse');
                 $('.socials').removeClass('inverse');
             }
+            startAnimation(newPageId);
         }
-    };      
+    };
 
-    /* menu */
-    var menu = $('.navbar__menu');
-    $('.menu__btn').on('click', function() {
-        toggleMenu(menu);
-    })
+    function toggleMenu(menu) {
+        menu.toggleClass('menu_on');
+        if (!menu.hasClass('menu_on'))
+            menu.toggleClass('menu_off');
+    };
+
+    function smoothScrollTo(offset) {
+        $('html, body').animate({
+            scrollTop: offset
+        }, 1000);
+        return false;
+    };
 
 
-};
-
-function toggleMenu(menu) {
-    menu.toggleClass('menu_on');
-    if (!menu.hasClass('menu_on'))
-        menu.toggleClass('menu_off');
-}
-function smoothScrollTo(offset) {
-  	$('html, body').animate({
-	  scrollTop: offset
-  	}, 1000);
-	return false;
 };
